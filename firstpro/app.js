a6190179c187b8583151d7c0a11460284cfcd15b
arifamrddn <74131332+arifamrddn@users.noreply.github.com>
2022-10-17 21:51:07 +0800

    meow

---
+++ firstpro/app.js
@@ -0,0 +1,99 @@
+// document.write('hellow worlds');
+// alert('hello worlds');
+// console.log('hello worlds');
+
+// document.write({ name: 'john' });
+// alert({ name: 'john' });
+// console.log({ name: 'john' });
+
+//variables - most basic building block
+//declare, assignment operator, assign value
+// let name = 'Arif Amiruddin bin Yaacob';
+// let address, zip, state;
+// address = '101 main stress ';
+// zip = '10100';
+// state = 'shah alom';
+///////////////////////////////////////////////////////////////////////////
+//variable
+//using var
+// var value = 'some balue'
+
+// //using let
+// let name = 'john'
+
+// //using const - cant re-assign
+// const lastName = 'jordan'
+
+// let firstName, last_name
+// firstName = 'Arif Amiruddin'
+// last_name = 'Yaacob'
+
+// let address = 'main street'
+// address = 'first street'
+// console.log(firstName, last_name, address)
+
+//////////////////////////////////////////////////////////////////////////
+// quotation marks
+
+// can use \' in a string untuk escape dia terstop
+
+//concat
+// const name = 'john'
+// const lastname = 'shakeandbake'
+// let fullname = name + ' ' + lastname
+
+// console.log('hello : ' + fullname)
+
+//backtick
+
+////////////////////////////////////////////////////////////////////////////
+// Numbers
+// Loosely Typed = dont declare type
+
+// const number = 34
+// const number2 = 2.456
+
+// const add = number + number2
+// const add2 = number - number2
+// const add3 = number * number2
+// const add4 = number / number2
+
+// let number = 40
+// number += 5 //=45
+
+// number++ //auto plus one
+
+//////////////////////////////////////////////////////////////////////////////
+// implicit
+
+// math operation on a string?
+const name = 'john'
+const lastName = 'jordan'
+const value = name - lastName //return nan
+
+let number3 = '10'
+let number4 = '20'
+const value = number3 - number4 //return -10 //js do convert
+
+/////////////////////////////////////////////////////////////////////////
+// Data types 7 total
+
+// typeof - operator  (typeof variable) (typeof value) //nak tau dia type apa
+
+//string
+const text = 'some text'
+
+//number
+const number = 24
+
+//Boolean
+let valu1 = true
+let valu2 = false
+
+//null
+let result = null
+
+//undefined
+let result2
+
+//symbol (ES6)
---
+++ firstpro/challenge3.js
@@ -0,0 +1,15 @@
+const score1 = 70
+const score2 = 12
+const score3 = 97
+
+let totalScore = score1 + score2 + score3
+let averageScore = totalScore / 3
+console.log(totalScore)
+console.log(averageScore)
+
+const plates = 20
+const people = 7
+
+let remaininPlates = plates % people
+remaininPlates++
+console.log('There are ' + remaininPlates + ' plates available')
