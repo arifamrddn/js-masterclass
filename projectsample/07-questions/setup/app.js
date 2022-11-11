b6f6d6181653ac57b4997b433ea76c8e6d923f60
arifamrddn <74131332+arifamrddn@users.noreply.github.com>
2022-11-11 18:38:39 +0800

    healing ke tu

--- projectsample/07-questions/setup/app.js
+++ projectsample/07-questions/setup/app.js
@@ -1,2 +1,22 @@
 //using selectors inside the element
-// traversing the dom
+
+const questions = document.querySelectorAll('.question')
+questions.forEach(function (question) {
+  const btn = question.querySelector('.question-btn') //cari button dalam satu div tu je terus kita dah limitkan. gini cara dia
+  btn.addEventListener('click', function () {
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove('show-text')
+      }
+    })
+    question.classList.toggle('show-text')
+  })
+})
+
+//traverse dom
+// const btns = document.querySelectorAll('.question-btn')
+// btns.forEach(function (btn) {
+//   btn.addEventListener('click', function () {
+//     btn.parentElement.parentElement.classList.toggle('show-text')
+//   })
+// })
